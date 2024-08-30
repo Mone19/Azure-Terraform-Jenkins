333c135103c442db34bb9b703c659116a45c7953
Mone19 <fuocoangelos@yahoo.de>
2024-08-30 10:23:00 +0200

    k8s test

--- main.tf
+++ main.tf
@@ -49,6 +49,15 @@ resource "azurerm_kubernetes_cluster" "aks" {
     vm_size    = "Standard_D2_v2"
     vnet_subnet_id = azurerm_subnet.jenkins_subnet.id
   }
+
+network_profile {
+  network_plugin = "azure"
+
+  service_cidr = "10.1.0.0/16"
+  dns_service_ip = "10.1.0.10"
+  docker_bridge_cidr = "172.17.0.1/16"
+}
+
   identity {
     type = "SystemAssigned"
   }
